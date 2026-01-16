e8078be83a60c702adc692ee439b1b1c98c7ddca
97254 <orzelig6@gmail.com>
2026-01-16 16:13:07 +0200

    chore: add dotenv config for mongo connection

--- src/db.js
+++ src/db.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://127.0.0.1:27017/postsdb');
+const uri = process.env.MONGO_URI;
+await mongoose.connect(uri);
 
 module.exports = mongoose;
