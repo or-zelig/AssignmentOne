1882f028036491c0b198f948e9fea28cd63eeee1
97254 <orzelig6@gmail.com>
2026-01-16 14:16:01 +0200

    add base app structure and mongo connection

---
+++ src/app.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+app.use('/post', require('./routes/posts.routes'));
+app.use('/comment', require('./routes/comments.routes'));
+
+app.listen(3000, () => console.log('Server running'));
